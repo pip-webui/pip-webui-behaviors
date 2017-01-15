e4affdefc88825843385eb9241e6787f1af11631
Anastas Fonotov <anastasf@gmail.com>
2017-01-15 23:33:39 +0200

    fix touch on pipSelected

--- src/selected/selected.ts
+++ src/selected/selected.ts
@@ -10,7 +10,7 @@
 
     var thisModule = angular.module("pipSelected", []);
 
-    thisModule.directive('pipSelected',function ($parse, $mdConstant, $timeout) {
+    thisModule.directive('pipSelected', function ($parse, $mdConstant, $timeout) {
         return {
             restrict: 'A',
             scope: false,
@@ -35,30 +35,30 @@
 
                 // Watch selected item index
                 if (!toBoolean($attrs.pipTreeList)) {
-                    $scope.$watch(indexGetter, function(newSelectedIndex) {
-                        selectItem({itemIndex: newSelectedIndex});
+                    $scope.$watch(indexGetter, function (newSelectedIndex) {
+                        selectItem({ itemIndex: newSelectedIndex });
                     });
                 } else {
-                    $scope.$watch(idGetter, function(newSelectedId) {
+                    $scope.$watch(idGetter, function (newSelectedId) {
                         setTimeout(function () {
-                            selectItem({itemId: newSelectedId, raiseEvent: true});
+                            selectItem({ itemId: newSelectedId, raiseEvent: true });
                         }, 0);
                     });
                 }
 
                 // Watch resync selection
                 if (pipSelectedWatch) {
-                    $scope.$watch(pipSelectedWatch, function() {
+                    $scope.$watch(pipSelectedWatch, function () {
                         // Delay update to allow ng-repeat to update DOM
-                        setTimeout(function() {
+                        setTimeout(function () {
                             selectedIndex = indexGetter($scope);
-                            selectItem({itemIndex: selectedIndex});
+                            selectItem({ itemIndex: selectedIndex });
                         }, 100);
                     });
                 }
 
                 // Select item defined by index
-                selectItem({itemIndex: selectedIndex, items: $element.find(className)});
+                selectItem({ itemIndex: selectedIndex, items: $element.find(className) });
 
                 // Converts value into boolean
                 function toBoolean(value) {
@@ -67,7 +67,7 @@
                     value = value.toString().toLowerCase();
                     return value == '1' || value == 'true';
                 };
-                
+
                 // Functions and listeners
 
                 function selectItem(itemParams) {
@@ -84,7 +84,7 @@
                             if (itemIndex >= 0 && itemIndex < itemsLength) {
                                 return items[itemIndex]
                             }
-                        }()),
+                        } ()),
                         raiseEvent = itemParams.raiseEvent;
 
                     if (item) {
@@ -139,7 +139,7 @@
                                     oldIndex: oldSelectedIndex
                                 }
                             });
-                        }                        
+                        }
                     }
                 };
 
@@ -155,10 +155,10 @@
                         itemBottom = itemTop + itemHeight,
                         containerScrollTop = $element.scrollTop();
 
-                        isScrolled = true;
-                        setTimeout(function () {
-                            isScrolled = false;
-                        }, 100);
+                    isScrolled = true;
+                    setTimeout(function () {
+                        isScrolled = false;
+                    }, 100);
 
                     if (containerTop > itemTop) {
                         $element.scrollTop(containerScrollTop + itemTop - containerTop);
@@ -169,9 +169,107 @@
 
                 };
 
-                $element.on('click touchstart', className, function (event) {
-                    selectItem({item: event.currentTarget, raiseEvent: true});
+                var touchStartX, touchStartY, trackingClick, trackingClickStart, targetElement, lastClickTime, cancelNextClick;
+
+                function getTargetElementFromEventTarget(eventTarget) {
+
+                    // On some older browsers (notably Safari on iOS 4.1 - see issue #56) the event target may be a text node.
+                    if (eventTarget.nodeType === Node.TEXT_NODE) {
+                        return eventTarget.parentNode;
+                    }
+
+                    return eventTarget;
+                };
+
+                function touchHasMoved(event) {
+                    var touch = event.changedTouches[0], boundary = 10; //Touchmove boundary, beyond which a click will be cancelled.
+
+                    if (Math.abs(touch.pageX - touchStartX) > boundary || Math.abs(touch.pageY - touchStartY) > boundary) {
+                        return true;
+                    }
+
+                    return false;
+                };
+
+                $element.on('click', className, function (event) {
+                    selectItem({ item: event.currentTarget, raiseEvent: true });
                 });
+
+                $element.on('touchstart', className, function (ev) {
+                    ev.preventDefault();
+                    console.log('touchstart event', event);
+
+                    event = ev.originalEvent;
+                    if (event['targetTouches'].length > 1) {
+                        return true;
+                    }
+                    let targetElement = getTargetElementFromEventTarget(event.target);
+                    let touch = event['targetTouches'][0];
+
+                    trackingClick = true;
+                    trackingClickStart = event.timeStamp;
+                    targetElement = targetElement;
+
+                    touchStartX = touch.pageX;
+                    touchStartY = touch.pageY;
+
+                    if ((event.timeStamp - lastClickTime) < 200) { // 200 - tapdelay
+                        event.preventDefault();
+                    }
+
+                    return true;
+                });
+
+                $element.on('touchmove', className, function (ev) {
+                    if (!trackingClick) {
+                        return true;
+                    }
+                    event = ev.originalEvent;
+                    // If the touch has moved, cancel the click tracking
+                    if (targetElement !== getTargetElementFromEventTarget(event.target) || touchHasMoved(event)) {
+                        trackingClick = false;
+                        targetElement = null;
+                    }
+
+                    return true;
+                });
+
+                $element.on('touchend', className, function (ev) {
+                    var forElement, newTrackingClickStart, targetTagName, scrollParent, touch, newtargetElement = targetElement;
+
+                    if (!trackingClick) {
+                        return true;
+                    }
+                    event = ev.originalEvent;
+                    // Prevent phantom clicks on fast double-tap (issue #36)
+                    if ((event.timeStamp - lastClickTime) < 200) { // 200 - tapdelay
+                        cancelNextClick = true;
+                        return true;
+                    }
+
+                    if ((event.timeStamp - trackingClickStart) > 700) { // tapTimeout - 700
+                        return true;
+                    }
+
+                    // Reset to prevent wrong click cancel on input (issue #156).
+                    cancelNextClick = false;
+
+                    lastClickTime = event.timeStamp;
+
+                    newTrackingClickStart = trackingClickStart;
+                    trackingClick = false;
+                    trackingClickStart = 0;
+
+                    selectItem({ item: ev.currentTarget, raiseEvent: true });
+
+                    return false;
+                });
+
+                $element.on('touchcancel', className, function (ev) {
+                    trackingClick = false;
+                    targetElement = null;
+                });
+
 
                 $element.on('keydown', function (e) {
                     var keyCode = e.which || e.keyCode;
@@ -192,20 +290,20 @@
                         }
 
                     } else
-                    if (keyCode == $mdConstant.KEY_CODE.LEFT_ARROW || keyCode == $mdConstant.KEY_CODE.RIGHT_ARROW ||
-                        keyCode == $mdConstant.KEY_CODE.UP_ARROW || keyCode == $mdConstant.KEY_CODE.DOWN_ARROW) {
+                        if (keyCode == $mdConstant.KEY_CODE.LEFT_ARROW || keyCode == $mdConstant.KEY_CODE.RIGHT_ARROW ||
+                            keyCode == $mdConstant.KEY_CODE.UP_ARROW || keyCode == $mdConstant.KEY_CODE.DOWN_ARROW) {
 
-                        e.preventDefault();
-                        e.stopPropagation();
+                            e.preventDefault();
+                            e.stopPropagation();
 
-                        // Get next selectable control index
-                        var items = $element.find(className + modifier),
-                            inc = (keyCode == $mdConstant.KEY_CODE.RIGHT_ARROW || keyCode == $mdConstant.KEY_CODE.DOWN_ARROW) ? 1 : -1,
-                            newSelectedIndex = selectedIndex + inc;
+                            // Get next selectable control index
+                            var items = $element.find(className + modifier),
+                                inc = (keyCode == $mdConstant.KEY_CODE.RIGHT_ARROW || keyCode == $mdConstant.KEY_CODE.DOWN_ARROW) ? 1 : -1,
+                                newSelectedIndex = selectedIndex + inc;
 
-                        // Set next control as selected
-                        selectItem({itemIndex: newSelectedIndex, items: items, raiseEvent: true});
-                    }
+                            // Set next control as selected
+                            selectItem({ itemIndex: newSelectedIndex, items: items, raiseEvent: true });
+                        }
                 });
 
                 $element.on('focusin', function (event) {
@@ -219,7 +317,7 @@
                     if (selectedItem.length === 0) {
                         selectedIndex = indexGetter($scope);
                         items = $element.find(className + modifier);
-                        selectItem({itemIndex: selectedIndex || 0, items: items, raiseEvent: true});
+                        selectItem({ itemIndex: selectedIndex || 0, items: items, raiseEvent: true });
                     }
                 });
 
