9886c26df1694b69ce7a46d2e537c1eabf1dd447
Anastas Fonotov <anastasf@gmail.com>
2017-05-17 19:52:21 +0300

    'rebuild'

--- dist/pip-webui-behaviors.d.ts
+++ dist/pip-webui-behaviors.d.ts
@@ -12,7 +12,6 @@ export interface IDraggableService {
 
 
 
-
 export interface IKeyboardShortcuts {
     [key: string]: Shortcut;
 }
@@ -82,6 +81,7 @@ export class Shortcut {
 
 
 export let ShortcutsChangedEvent: string;
+
 
 
 }
